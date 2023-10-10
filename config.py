74b5ad0e05e0362e3e368b7d25d5cee9cc8c4bee
Joel Savitz <joel@kdlp.underground.software>
2023-10-10 01:56:59 -0400

    fix config
    Signed-off-by: Joel Savitz <joel@kdlp.underground.software>

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 #!/bin/env python3
 import os
 
-srvname     = 'kdlp.underground.software'
+srvname     = 'balance.charity'
 refresh     = 0
 
 appname     = 'balance'
 version     = '0.1'
-source      = 'https://github.com/underground-software/balance.charity'
+source      = 'https://github.com/theyoyojo/balance.charity'
 
 radius_port = '9098'
 
 
-orbit_root   = '/home/joel/BALANCE/thebalancejerusalem.org'
+orbit_root   = '/var/balance.charity'
 
 # read these documents from a filesystem path
 dataroot    = f'{orbit_root}/docs'
