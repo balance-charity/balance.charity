b807e0a75db2b7c27db3e7a4aa93529722b3b98b
Joel Savitz <joelsavitz@gmail.com>
2023-10-10 13:07:48 +0300

    we are bootstrapping
    Signed-off-by: Joel Savitz <joelsavitz@gmail.com>

--- config.py
+++ config.py
@@ -2,14 +2,12 @@
 import os
 
 srvname     = 'balance.charity'
-refresh     = 0
 
 appname     = 'balance'
 version     = '0.1'
 source      = 'https://github.com/theyoyojo/balance.charity'
 
 radius_port = '9098'
-
 
 orbit_root   = '/var/balance.charity'
 
@@ -22,8 +20,8 @@ style   = f'{dataroot}/style.css'
 
 title       = 'The Balance'
 nav_buttons = [
-    ('/home', 'Home'     ),
-    ('/about', 'About Us' ),
-    ('/testimonials', 'Testimonials'),
-    ('/faq', 'FAQ'),
-    ('/join', 'Get Involved')]
+    ('/home', 		'Home'     ),
+    ('/about', 		'About Us' ),
+    ('/testimonials', 	'Testimonials'),
+    ('/faq', 		'FAQ'),
+    ('/join', 		'Get Involved')]
--- server.py
+++ server.py
@@ -179,8 +179,11 @@ class Rocket:
         msgdoc += [('source',   cfg.source)]
         # Concatenate all components to complete this format operation
         output = ''
-        output += mk_style()
         output += mk_chrset()
+        # boostrap
+        output += '<meta name="viewport" content="width=device-width, initial-scale=1">'
+        output += '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">'
+        output += mk_style()
         output += logo_div_gen()
         output += nav_div_gen()
         output += doc
