7bd93f488e4c0ef9bc9d6fdceafd2ffe41985144
Joel Savitz <joel@kdlp.underground.software>
2023-10-09 23:20:37 -0400

    create new website for the balance: initial stubs
    Signed-off-by: Joel Savitz <joel@kdlp.underground.software>

---
+++ config.py
@@ -0,0 +1,30 @@
+#!/bin/env python3
+import os
+
+srvname     = 'kdlp.underground.software'
+refresh     = 0
+
+appname     = 'balance'
+version     = '0.1'
+source      = 'https://github.com/underground-software/balance'
+docsrc      = 'https://github.com/underground-software/thebalancejerusalem.org'
+
+radius_port = '9098'
+
+
+orbit_root   = '/home/joel/BALANCE/thebalancejerusalem.org'
+
+# read these documents from a filesystem path
+dataroot    = f'{orbit_root}/docs'
+
+logo    = f'{dataroot}/logo.png'
+style   = f'{dataroot}/style.css'
+
+
+title       = 'The Balance'
+nav_buttons = [
+    ('/home', 'Home'     ),
+    ('/about', 'About Us' ),
+    ('/testimonials', 'Testimonials'),
+    ('/faq', 'FAQ'),
+    ('/join', 'Get Involved')]
